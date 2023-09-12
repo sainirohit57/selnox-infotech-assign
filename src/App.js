b1f685d45937361bf0a8d0491d58e90fb6b6f4dc
Rohit Saini <rohit@techfnatic.com>
2023-09-12 11:01:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 mt-5">
+            <EmployeeList />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
